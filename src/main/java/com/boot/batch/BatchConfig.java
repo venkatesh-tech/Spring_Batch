0c7357139b4a5084f4dee87cb1c163bc5dd1c3b0
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-11-01 11:24:54 +0530

    Configured Step & Job

--- src/main/java/com/boot/batch/BatchConfig.java
+++ src/main/java/com/boot/batch/BatchConfig.java
@@ -1,9 +1,42 @@
 package com.boot.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BatchConfig {
 
+	@Autowired
+	private StepBuilderFactory sbf;
+	
+	@Autowired
+	private JobBuilderFactory jbf;
+	
+	@Bean
+	public Job job(){
+		return jbf.get("job1")
+				.incrementer(new RunIdIncrementer())
+				.listener(listener())
+				.start(step())
+				.build();
+	}
+	
+	@Bean
+	public Step step() { //method name can also be step1, step2, step3
+		return sbf.get("step1")
+				.<String,String>chunk(1)
+				.reader(reader())
+				.writer(writer())
+				.processor(processor())
+				.build();
+	}
+	
 	@Bean
 	public Reader reader() {
 		return new Reader();
